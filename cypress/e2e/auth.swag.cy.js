3e45c3fe750fdcc886c5366c01cc3b69eb389ec0
Beatriz Sara <bea.sara.santos@outlook.com>
2026-02-04 22:34:02 -0300

    fix: correção de comentario linha 10

--- cypress/e2e/auth.swag.cy.js
+++ cypress/e2e/auth.swag.cy.js
@@ -7,7 +7,7 @@ describe('Login', () => {
      // Tela de login
     cy.get('.login_logo').should('exist')
 
-    //Login
+    //Realiza Login
     cy.get('[data-test="username"]').type('standard_user')
     cy.get('[data-test="password"]').type('secret_sauce')
     cy.get('[data-test="login-button"]').click()
